f7995d3850d66f31d2b35b5d0ef02b11d27564eb
JankenLv <814076827@qq.com>
2018-11-22 00:11:45 +0800

    ActiveMQ学习中。。。

---
+++ ActiveMQ_demo/src/main/java/com/ljs/jms/demo/service/ProducerService.java
@@ -0,0 +1,30 @@
+package com.ljs.jms.demo.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.jms.Destination;
+
+/**
+ * 功能描述：消息生产
+ *
+ * <p> @作者：jankin_lv </p>
+ * <p> @创建时间: 2018/11/21 22:59 </p>
+ */
+@Component
+public interface ProducerService {
+
+    /**
+     * 功能描述：指定消息队列，还有消息
+     * @param destination
+     * @param message
+     */
+    void sendMessage(Destination destination, final String message);
+
+   /**
+    * 功能描述：使用默认消息队列，发送消息
+    * @param message
+    * @return void
+    */
+    void sendMessage(final String message);
+
+}
