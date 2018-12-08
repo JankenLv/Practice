573859942c7e5c8ee1b2cac6aade7c1ff1507d46
JankenLv <814076827@qq.com>
2018-12-08 22:28:17 +0800

    创建rocket_demo项目

---
+++ RocketMQ_Demo/src/main/java/rocketmq/demo/RocketMqDemoApplication.java
@@ -0,0 +1,12 @@
+package rocketmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketMqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqDemoApplication.class, args);
+    }
+}
