a66da12681dc859729bdaa5495a871f1d7cb22f6
JankenLv <814076827@qq.com>
2018-12-11 23:15:24 +0800

    WebFlux响应式编程Demo

--- WebFlux_Demo/src/test/java/com/webflux/demo/DemoApplicationTests.java
+++ WebFlux_Demo/src/test/java/com/webflux/demo/DemoApplicationTests.java
@@ -3,14 +3,21 @@ package com.webflux.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 public class DemoApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void testGetUser() {
+        Mono<String> mono = WebClient.create().get()
+            .uri("http://localhost:8080/api/v1/user/get?id={id}", "5")
+            .accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class);
+        System.out.println("====>>测试结果：" + mono.block());
     }
 
 }
